192425bcbb7e46ed115a753fb7c962b0b9b51f99
ahh1539 <ahh1539@g.rit.edu>
2021-07-06 22:12:10 -0400

    added dfyn

--- crypto_scraper.py
+++ crypto_scraper.py
@@ -13,16 +13,25 @@ sh = gc.open_by_key(document_id)
 worksheet = sh.sheet1
 res = worksheet.get_all_records()
 
-beefy_vault = requests.get('https://api.apeboard.finance/beefyMatic/{}'.format(wallet_address)).json()
+# beefy_vault = requests.get('https://api.apeboard.finance/beefyMatic/{}'.format(wallet_address)).json()
+dfyn_vault = requests.get('https://api.apeboard.finance/dfynMatic/{}'.format(wallet_address)).json()
 matic_wallet = requests.get('https://api.apeboard.finance/wallet/matic/{}'.format(wallet_address)).json()
 eth_wallet = requests.get('https://api.apeboard.finance/wallet/eth/{}'.format(wallet_address)).json()
 
 def calculate_vaults(all_vaults):
     all_vaults_total = 0
     for vault in all_vaults:
-        for position in vault.get('positions'):
-            for token in position.get('tokens'):
-                all_vaults_total += (float(token.get('balance')) * float(token.get('price')))
+        if vault.get('positions'):
+            for position in vault.get('positions'):
+                for token in position.get('tokens'):
+                    all_vaults_total += (float(token.get('balance')) * float(token.get('price')))
+        else:
+            for position in vault.get('farms'):
+                for token in position.get('tokens'):
+                    all_vaults_total += (float(token.get('balance')) * float(token.get('price')))
+            if position.get('rewards'):
+                for token in position.get('rewards'):
+                    all_vaults_total += (float(token.get('balance')) * float(token.get('price')))
     return all_vaults_total
 
 def calculate_wallets(all_wallets):
@@ -34,7 +43,7 @@ def calculate_wallets(all_wallets):
 
 def main():
     total_value = 0
-    total_value += calculate_vaults([beefy_vault])
+    total_value += calculate_vaults([dfyn_vault])
     total_value += calculate_wallets([eth_wallet, matic_wallet])
     data = [date.today().strftime("%m/%d/%y").strip(), total_value]
     worksheet.append_row(data)
